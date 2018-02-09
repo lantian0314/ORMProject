0c5b3bdacad85f2c04e6101d3f82ec3fb3de0d9e
xingyatong <384289339@qq.com>
2018-02-09 18:14:16 +0800

    Initial commit

---
+++ app/src/main/java/orm/ormproject/db/Student.java
@@ -0,0 +1,11 @@
+package orm.ormproject.db;
+
+import com.orm.SugarRecord;
+
+import java.io.Serializable;
+
+/**
+ * Created by xingyatong on 2018/2/9.
+ */
+public class Student extends SugarRecord implements Serializable {
+}
